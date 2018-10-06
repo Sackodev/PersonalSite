5e7b5403bd2209a20753727ba3101131077bf392
wocuangle <wocuangle@gmail.com>
2018-10-06 16:08:16 -0500

    I figured out how to use effects in GIMP to make images appear how I wanted them to, so I changed the images to reflect that. I also added some pictures to my projects, and added a project. The projects table has a new column to store edited image filenames, so users can click on the project page's images to get the unedited version if they wanted to see them. I also changed some of the styling to make the top of the page smaller.

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,38 @@
 from flask import Flask, render_template, request, session
-import sqlite3, csv
+import sqlite3
+import csv
 
 app = Flask(__name__)
 
 conn = sqlite3.connect('../ZachSiteDB.db')
-conn.execute('''CREATE TABLE if not exists projects (projid INTEGER PRIMARY KEY, projname TEXT NOT NULL, desc TEXT NOT NULL, link TEXT NOT NULL, img TEXT);''')
+conn.execute('''CREATE TABLE if not exists projects (projid INTEGER PRIMARY KEY, projname TEXT NOT NULL, desc TEXT NOT NULL, link TEXT NOT NULL, img TEXT, editimg TEXT);''')
 
 with open('app/static/csv/projects.csv') as csvfile:
     curs = conn.cursor()
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for csvrow in reader:
-        curs.execute('SELECT * FROM projects WHERE projname = ? LIMIT 1', (csvrow[0],))
+        curs.execute(
+            'SELECT * FROM projects WHERE projname = ? LIMIT 1', (csvrow[0],))
         trow = curs.fetchone()
+
         if trow is None:
-            t = (csvrow[0], csvrow[1], csvrow[2], csvrow[3])
-            curs.execute('INSERT INTO projects (projname, desc, link, img) VALUES (?,?,?,?)', t)
-        elif (csvrow[0] != trow[1] or csvrow[1] != trow[2] or csvrow[2] != trow[3] or csvrow[3] != trow[4]):
-            t = (csvrow[0], csvrow[1], csvrow[2], csvrow[3], trow[0])
-            curs.execute('UPDATE projects SET projname=?, desc=?, link=?, img=? WHERE projid=?', t)
+            t = (csvrow[0], csvrow[1], csvrow[2], csvrow[3], csvrow[4])
+            curs.execute(
+                'INSERT INTO projects (projname, desc, link, img, editimg) VALUES (?,?,?,?,?)', t)
+        elif (csvrow[0] != trow[1] or csvrow[1] != trow[2] or csvrow[2] != trow[3] or csvrow[3] != trow[4] or csvrow[4] != trow[5]):
+            t = (csvrow[0], csvrow[1], csvrow[2],
+                 csvrow[3], csvrow[4], trow[0])
+            curs.execute(
+                'UPDATE projects SET projname=?, desc=?, link=?, img=?, editimg=? WHERE projid=?', t)
 
 conn.commit()
 conn.close()
 
+
 @app.route('/')
 def home():
     return render_template('layoutBox.html')
+
 
 @app.route('/', methods=['POST'])
 def process():
@@ -37,22 +45,26 @@ def process():
     elif (hashID == "home"):
         return defaultCont()
 
+
 def projects():
-   con = sqlite3.connect("../ZachSiteDB.db")
-   con.row_factory = sqlite3.Row
+    con = sqlite3.connect("../ZachSiteDB.db")
+    con.row_factory = sqlite3.Row
 
-   cur = con.cursor()
-   cur.execute("SELECT * FROM projects")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM projects")
 
-   rows = cur.fetchall();
-   return render_template("projects.html",rows = rows)
+    rows = cur.fetchall()
+    return render_template("projects.html", rows=rows)
+
 
 def resume():
     return render_template("resume.html")
 
+
 @app.route('/default')
 def defaultCont():
     return render_template('contentDefault.html')
+
 
 if __name__ == '__main__':
     app.secret_key = 'oihg49whg7hw4gi'
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,41 +1,49 @@
 $(document).ready(function() {
     $("#searchInput").focus();
 
+    const SEARCH_INPUT_ID = "#searchInput";
+
     var pages = ["home", "projects", "resume"];
 
     console.log(pages);
 
-    if(window.location.hash){
-      console.log(window.location.hash);
-      if(pages.indexOf(window.location.hash.substring(1)) > -1){
-          console.log(1);
-          $("#content").load("/", {hashID: window.location.hash.substring(1)});
-      } else {
-          console.log(2);
-          $("#content").load("/default");
-          location.hash = "home";
-      }
+    if (window.location.hash) {
+        console.log(window.location.hash);
+        if (pages.indexOf(window.location.hash.substring(1)) > -1) {
+            console.log(1);
+            $("#content").load("/", {
+                hashID: window.location.hash.substring(1)
+            });
+        } else {
+            console.log(2);
+            $("#content").load("/default");
+            location.hash = "home";
+        }
     } else {
-      $("#content").load("/default");
+        $("#content").load("/default");
     }
 
     $("#searchButton").click(function() {
-      if(pages.indexOf($("#searchInput").val().toLowerCase()) > -1){
-        location.hash = $("#searchInput").val().toLowerCase();
-      }
-      $("#searchInput").val("");
-    });
-    $("#searchInput").on('keyup', function(e) {
-      if (e.keyCode == 13) {
-        if(pages.indexOf($("#searchInput").val().toLowerCase()) > -1){
-          location.hash = $("#searchInput").val().toLowerCase();
+        var searchInputText = $(SEARCH_INPUT_ID).val().toLowerCase();
+        if (pages.indexOf(searchInputText) > -1) {
+            location.hash = searchInputText;
         }
-        $("#searchInput").val("");
-      }
+        $(SEARCH_INPUT_ID).val("");
+    });
+    $(SEARCH_INPUT_ID).on('keyup', function(e) {
+        var searchInputText = $(SEARCH_INPUT_ID).val().toLowerCase();
+        if (e.keyCode == 13) {
+            if (pages.indexOf(searchInputText) > -1) {
+                location.hash = searchInputText;
+            }
+            $(SEARCH_INPUT_ID).val("");
+        }
     });
 
-    $(window).on('hashchange', function(){
-      $("#content").load("/", {hashID: window.location.hash.substring(1)});
+    $(window).on('hashchange', function() {
+        $("#content").load("/", {
+            hashID: window.location.hash.substring(1)
+        });
     });
 
-  });
+});
